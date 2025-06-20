08b785005a57c4d4ef8d6c531870ae08b23d70a1
Getpeca.ai <Getpeca.ai@gmail.com>
2025-06-20 13:38:08 +0530

    Disable social account backend in authentication settings

--- gynecology_chatbot/backend/gynecology_chatbot_project/settings.py
+++ gynecology_chatbot/backend/gynecology_chatbot_project/settings.py
@@ -114,7 +114,7 @@ AUTH_USER_MODEL = 'users.User'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
-    'allauth.socialaccount.auth_backends.SocialAccountBackend',
+    # 'allauth.socialaccount.auth_backends.SocialAccountBackend',
 ]
 
 # Login/Logout URLs
